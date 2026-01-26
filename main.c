1d90725cece0d4705b59299c531852e7d5ce02d5
Nikita <cold17748@gmail.com>
2026-01-27 01:39:12 +0300

    Add batch_sum, linear_update_grad

--- main.c
+++ main.c
@@ -71,6 +71,10 @@ extern void linear_forward(struct Linear* module);
 
 extern void memcopy(void* dst, void* src, size_t n);
 
+extern void batch_sum(const float* M, float* v, size_t n, size_t m);
+
+extern void linear_update_grad(struct Linear* module);
+
 void test_linear_forward() {
     float bias[3] = {1, 2, 3};
     float weight[6 * 3] = {0.1, 1.1, 2.1, 0.2, 1.2, 2.2, 0.3, 1.3, 2.3,
@@ -100,6 +104,45 @@ void test_linear_forward() {
     puts("");
 }
 
+void test_linear_update_grad() {
+    float bias[3] = {1, 2, 3};
+    float weight_t[3 * 6] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 1.1, 1.2, 1.3,
+                             1.4, 1.5, 1.6, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6};
+    float input[2 * 6] = {1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 16};
+    float grad_input[2 * 6] = {0};
+    float grad_output[2 * 3] = {2.1, 2.2, 2.3, 2.4, 2.5, 2.6};
+    float grad_bias[3] = {0};
+    float grad_weight[3 * 6] = {0};
+    struct Linear module = {.weight = weight_t,
+                            .weight_m = 3,
+                            .weight_n = 6,
+                            .bias = bias,
+                            .bias_n = 3,
+                            .input = input,
+                            .input_m = 2,
+                            .input_n = 6,
+                            .output_m = 2,
+                            .output_n = 3,
+                            .grad_input = grad_input,
+                            .grad_output = grad_output,
+                            .grad_weight = grad_weight,
+                            .grad_bias = grad_bias};
+
+    linear_update_grad(&module);
+
+    puts("Grad output");
+    for (int i = 0; i < 2 * 6; ++i)
+        printf("%f, ", grad_input[i]);
+    puts("\nGrad weight");
+    for (int i = 0; i < 3 * 6; ++i)
+        printf("%f, ", grad_weight[i]);
+    puts("\nGrad output");
+    for (int i = 0; i < 3; ++i)
+        printf("%f, ", grad_bias[i]);
+    puts("");
+}
+
 int main() {
+    test_linear_update_grad();
     return 0;
 }
