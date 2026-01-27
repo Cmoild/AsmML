ff2d8b449a1e532f6a440c75f8760e1f5c928249
Nikita <cold17748@gmail.com>
2026-01-28 02:56:24 +0300

    Add relu_forward, relu_update_grad

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ extern void matmul_t_naive(const float* A, const float* B, float* C, size_t m, s
 extern void matmul_left_t_naive(const float* A, const float* B, float* C, size_t m, size_t k,
                                 size_t n);
 
-extern void relu(float* M, size_t n);
+extern void relu(float* M, size_t n, float* M_out);
 
 extern void exp8fv(float* V);
 
@@ -74,6 +74,26 @@ extern void memcopy(void* dst, void* src, size_t n);
 extern void batch_sum(const float* M, float* v, size_t n, size_t m);
 
 extern void linear_update_grad(struct Linear* module);
+
+struct ReLU {
+    float* input;
+    size_t input_m;
+    size_t input_n;
+    float* output;
+    float* grad_input;
+    float* grad_output;
+};
+
+_Static_assert(offsetof(struct ReLU, input) == 0, "");
+_Static_assert(offsetof(struct ReLU, input_m) == 8, "");
+_Static_assert(offsetof(struct ReLU, input_n) == 16, "");
+_Static_assert(offsetof(struct ReLU, output) == 24, "");
+_Static_assert(offsetof(struct ReLU, grad_input) == 32, "");
+_Static_assert(offsetof(struct ReLU, grad_output) == 40, "");
+
+extern void relu_forward(struct ReLU* module);
+
+extern void relu_update_grad(struct ReLU* module);
 
 void test_linear_forward() {
     float bias[3] = {1, 2, 3};
@@ -142,7 +162,39 @@ void test_linear_update_grad() {
     puts("");
 }
 
+void test_relu_forward() {
+    float test[25] = {0.1072, 0.1223, -1.2947, -0.2245, 0.2226,  0.6319,  -1.1761, -2.0376, -1.4152,
+                      0.0078, 0.4336, 0.3302,  -1.1719, -0.5094, -0.6923, 0.0433,  1.5905,  -0.0333,
+                      0.1904, 0.9133, 0.5219,  -0.8280, -0.5921, 1.1436,  -1.2585};
+    float res[25] = {0};
+    struct ReLU module = {.input = test, .input_m = 5, .input_n = 5, .output = res};
+    relu_forward(&module);
+    for (int i = 0; i < 25; i++)
+        printf("%f, ", res[i]);
+    puts("");
+}
+
+void test_relu_update_grad() {
+    float test[25] = {0.9604,  -0.3194, -1.0436, -0.2665, -0.3246, 1.5302,  -0.3800, 1.3150, 1.3504,
+                      -2.7083, -1.4846, -0.0906, 2.0149,  0.8364,  -2.3374, -0.0776, 0.9906, 0.3975,
+                      -1.7088, 0.1813,  1.3356,  -0.7327, 0.8508,  0.7693,  -0.9871};
+    float grad_input[25] = {0};
+    float grad_output[25] = {1.5683,  -1.0189, 1.0044,  -1.2993, -1.1357, 0.9425,  -0.5050,
+                             2.3649,  -0.9042, 0.2715,  -0.2236, 0.6351,  -1.0446, 0.1262,
+                             1.1680,  -0.8709, 0.0865,  2.8323,  0.5354,  -0.8447, 2.3348,
+                             -1.6300, -0.0472, -1.3245, 0.5434};
+    struct ReLU module = {.input = test,
+                          .input_m = 5,
+                          .input_n = 5,
+                          .grad_input = grad_input,
+                          .grad_output = grad_output};
+    relu_update_grad(&module);
+    for (int i = 0; i < 25; i++)
+        printf("%f, ", grad_input[i]);
+    puts("");
+}
+
 int main() {
-    test_linear_update_grad();
+    test_relu_update_grad();
     return 0;
 }
